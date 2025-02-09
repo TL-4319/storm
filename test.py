1f5eaf21080f24b2445a57b2e7cdbfdf8c9d0125
Tuan Luong <tdluong@crimson.ua.edu>
2025-02-09 17:53:48 -0600

    setup container and include example to support dev work

---
+++ test.py
@@ -0,0 +1,8 @@
+import carbs.src.carbs.utilities
+from carbs.src.carbs.swarm_estimator.tracker import ProbabilityHypothesisDensity
+from carbs.test.validation.swarm_estimator import test_tracker
+
+
+res = test_tracker.main()
+
+
